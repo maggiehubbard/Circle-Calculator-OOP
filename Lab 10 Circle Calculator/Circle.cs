071e3fa10f9a57c3aa338355e9265a224c052c13
maggiehubbard <maggiehubbard@wayne.edu>
2018-05-03 20:58:30 -0400

    Initial Commit

---
+++ Lab 10 Circle Calculator/Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10_Circle_Calculator
+{
+    class Circle
+    {
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        private double _radius;
+        public double Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public static int Counter;
+
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return CalculateCircumference().ToString("F");//.ToString("F") formats outputs to have 2 decimal places
+        }
+
+        public double CalculateArea()
+        {
+            return (Math.PI * (Radius * Radius));
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return CalculateArea().ToString("F");
+        }
+
+        private string FormatNumber(double x)
+        {
+            return x.ToString("F");
+        }
+
+    }
+}
---
+++ Lab 10 Circle Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10_Circle_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radNum;
+            string response = "y";
+            Console.WriteLine("Welcome to the Circle Tester: ");
+            Console.WriteLine("===================================\n");
+
+            while (response.ToLower() == "y")
+            {
+                Console.WriteLine("Enter radius: ");
+                string rad = Console.ReadLine();
+                bool valid = double.TryParse(rad, out radNum);
+                while (!valid) //while loop to validate input was a number
+                {
+                    Console.WriteLine("Error: The input was not valid. ");
+                    Console.WriteLine("Enter radius: ");
+                    rad = Console.ReadLine();
+                    valid = double.TryParse(rad, out radNum);
+                }
+
+                Circle circle1 = new Circle(radNum);//creates new instance (object) of circle class
+                Circle.Counter++;
+
+                //displays the circumference and area
+                Console.WriteLine($"The radius of your cirlce is: {circle1.Radius}");
+                Console.WriteLine($"The area of your cirlce is: {circle1.CalculateFormattedArea()}");
+                Console.WriteLine($"The circumference of your cirlce is: {circle1.CalculateFormattedCircumference()}");
+                Console.WriteLine("Would you like to continue? (y/n)");
+                response = Console.ReadLine().ToLower();
+            }
+            //Pause
+            Console.WriteLine($"You created {Circle.Counter} circle object(s)");
+            Console.WriteLine("Goodbye!\nPress any key to exit . .");
+            Console.ReadKey();
+
+        }
+    }
+}
